777b58f4c4b95c72e7098909cc713948ff5880ce
Roshan Kumar <roskumr@amazon.com>
2018-11-30 15:14:26 -0800

    Update the config and scriptfiles

--- circleciscript.sh
+++ circleciscript.sh
@@ -1,5 +1,9 @@
+git clone https://github.com/awslabs/aws-sdk-android-samples.git android-samples-1
+cfolder=$(pwd)
+cd android-samples-1/
+git checkout mobileClient
 cd S3TransferUtilitySample/
 mkdir res/raw/
-cp ~/awsconfiguration.json res/raw/awsconfiguration.json
+cp $cfolder/awsconfiguration.json res/raw/awsconfiguration.json
 ./gradlew cAT
-
+cd $cfolder
